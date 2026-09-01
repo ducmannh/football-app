6023dadafc8c7b412d78bc799266fb76ccacf000
ducmanh <tranducmanh2342000@gmail.com>
2026-09-02 02:06:13 +0700

    update6

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL || "postgresql://postgres:123456@postgres:5432/my_football_db?schema=public",
   },
 });
